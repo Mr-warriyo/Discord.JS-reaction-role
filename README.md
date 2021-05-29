5205aeba17c7943da9c03a288ed6bc486c1a1c59
Mr-warriyo <78315967+Mr-warriyo@users.noreply.github.com>
2021-05-29 20:02:35 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Start
+npm start
---
+++ index.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+})
+
+const ReactionRoleManager = require("djs-reaction-role");
+const manager = new ReactionRoleManager(client, {
+    storage: "./role.json"
+});
+client.reactionRoleManager = manager;
+
+client.on("ready", () => {
+    console.log("Reaction Role is ready!");
+});
+
+client.on('message', message => {
+    if (message.content === 'react') {
+        client.reactionRoleManager.create({
+      messageID: '847778688962789406',
+      channel: message.channel,
+      reaction: '✅',
+      role: message.guild.roles.cache.get('847778770551177216')
+     })
+    }
+})
+client.login('ODQ3Nzc2MTEzODQ0Mjg5NTY2.YLC_CA.k_CmsqyNsQcuRu7BF9Asd2TEeXE')
